fc80d17ae7aca24f2ef04c7030a5a2fd6939d608
SaulFromRobots <143561329+SaulFromRobots@users.noreply.github.com>
2023-09-05 16:15:55 -0700

    add rollcall.py

---
+++ rollcall.py
@@ -0,0 +1,7 @@
+# the team 668 coding style involves excessive comments
+names = [ # create a list to store the names of the people participating in the workshop
+	"placeholder" # write a fun fact about yourself!
+]
+
+for name in names: # iterate (as name) over the values in names
+	print(name) # print out the name
